ac899b2421ef86d45c767a1a02cd2115ca422a51
Efim Golovin <golovin.e@selectel.ru>
2022-12-08 02:54:00 +0300

    Initial commit

--- source/iris_code/lightgbm_training.py
+++ source/iris_code/lightgbm_training.py
@@ -4,11 +4,25 @@ from sklearn.datasets import load_iris
 from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split
 
-from clearml import Task
+from sklearn.metrics import precision_score, recall_score, f1_score
+
+import pandas as pd
 
 import os
 
-Task.init('MASTER-CLASS/Iris', 'LightGBM training')
+from clearml import Task
+from clearml import Logger
+
+params = {
+    'objective': 'multiclass',
+    'metric': 'softmax',
+    'num_class': 3,
+    'random_state': 1,
+    'test_size': 0.2
+}
+
+task = Task.init('MASTER-CLASS/Iris', 'LightGBM training', tags=['lightgbm'])
+task.connect(params)
 
 model_dir = "."
 BST_FILE = "model.bst"
@@ -18,24 +32,29 @@ iris = load_iris()
 y = iris['target']
 X = iris['data']
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
-X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.25, random_state=1)  # 0.25 x 0.8 = 0.2
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y,
+    test_size=params['test_size'],
+    random_state=params['random_state'],
+    stratify=y
+)
+X_train, X_val, y_train, y_val = train_test_split(
+    X_train, y_train,
+    test_size=0.25,
+    random_state=params['random_state'],
+    stratify=y_train
+)  # 0.25 x 0.8 = 0.2
 
 
 dtrain = lgb.Dataset(X_train, label=y_train)
 dval = lgb.Dataset(X_val, label=y_val)
 dtest = lgb.Dataset(X_test, label=y_test)
 
-params = {
-    'objective': 'multiclass',
-    'metric': 'softmax',
-    'num_class': 3
-}
-
 lgb_model = lgb.train(
-    params=params,
+    params={k: v for k, v in params.items() if k not in ['random_state', 'test_size']},
     train_set=dtrain,
-    valid_sets=dval
+    valid_sets=dval,
+    valid_names='evaluation'
 )
 
 model_file = os.path.join(model_dir, BST_FILE)
@@ -54,4 +73,31 @@ target_names = [
     'Iris Virginica'
 ]
 
-print(classification_report(y_test, y_pred, target_names=target_names))
+classification_report_dict = classification_report(y_test, y_pred, target_names=target_names, output_dict=True)
+
+df = pd.DataFrame(data=classification_report_dict).T
+
+Logger.current_logger().report_table(
+    title='Classification report',
+    series='',
+    iteration=0,
+    table_plot=df
+)
+Logger.current_logger().report_scalar(
+    title='precision',
+    series='macro',
+    value=precision_score(y_test, y_pred, average="macro"),
+    iteration=0
+)
+Logger.current_logger().report_scalar(
+    title='recall',
+    series='macro',
+    value=recall_score(y_test, y_pred, average="macro"),
+    iteration=0
+)
+Logger.current_logger().report_scalar(
+    title='f1',
+    series='macro',
+    value=f1_score(y_test, y_pred, average="macro"),
+    iteration=0
+)
---
+++ source/iris_code/sklearn_lr_training.py
@@ -0,0 +1,97 @@
+try:
+    import joblib
+except ImportError:
+    from sklearn.externals import joblib
+
+from sklearn.metrics import precision_score, recall_score, f1_score
+from sklearn.datasets import load_iris
+from sklearn.metrics import classification_report
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+
+import pandas as pd
+
+import os
+
+from clearml import Task
+from clearml import Logger
+
+params = {
+    'solver': 'liblinear',
+    'multi_class': 'auto',
+    'max_iter': 100,
+    'random_state': 1,
+    'test_size': 0.2,
+    'fit_intercept': True
+}
+
+task = Task.init('MASTER-CLASS/Iris', 'Scikit-Learn training: LogisticRegression', tags=['scikit-learn'])
+task.connect(params)
+
+model_dir = "."
+BST_FILE = "model.joblib"
+
+iris = load_iris()
+
+y = iris['target']
+X = iris['data']
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y,
+    test_size=params['test_size'],
+    random_state=params['random_state'],
+    stratify=y
+)
+
+model = LogisticRegression(
+    solver=params['solver'],
+    multi_class=params['multi_class'],
+    max_iter=params['max_iter'],
+    random_state=params['random_state'],
+    fit_intercept=params['fit_intercept']
+)  # sklearn LogisticRegression class
+
+model.fit(X_train, y_train)
+
+model_file = os.path.join(model_dir, BST_FILE)
+
+joblib.dump(model, model_file, compress=True)
+
+loaded_model = joblib.load(model_file)
+
+y_pred = loaded_model.predict(X_test)
+
+target_names = [
+    'Iris Setosa',
+    'Iris Versicolour',
+    'Iris Virginica'
+]
+
+classification_report_dict = classification_report(y_test, y_pred, target_names=target_names, output_dict=True)
+
+df = pd.DataFrame(data=classification_report_dict).T
+
+Logger.current_logger().report_table(
+    title='Classification report',
+    series='',
+    iteration=0,
+    table_plot=df
+)
+Logger.current_logger().report_scalar(
+    title='precision',
+    series='macro',
+    value=precision_score(y_test, y_pred, average="macro"),
+    iteration=0
+)
+Logger.current_logger().report_scalar(
+    title='recall',
+    series='macro',
+    value=recall_score(y_test, y_pred, average="macro"),
+    iteration=0
+)
+Logger.current_logger().report_scalar(
+    title='f1',
+    series='macro',
+    value=f1_score(y_test, y_pred, average="macro"),
+    iteration=0
+)
