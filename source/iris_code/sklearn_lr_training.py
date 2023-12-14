7bf861ef970ca87a10fb3852263de1ec785cfd54
alekseev.aa <alekseev.aa@selectel.ru>
2023-12-14 11:54:52 +0300

    fix project name

--- source/iris_code/sklearn_lr_training.py
+++ source/iris_code/sklearn_lr_training.py
@@ -25,7 +25,7 @@ params = {
     'fit_intercept': True
 }
 
-task = Task.init('DEMO/Iris 1', 'Scikit-Learn training: LogisticRegression', tags=['scikit-learn'])
+task = Task.init('DEMO/Iris', 'Scikit-Learn training: LogisticRegression', tags=['scikit-learn'])
 # task.execute_remotely(queue_name="cpu_scheduler", exit_process=True)
 task.connect(params)
 
